745100f9f1b47ae69b65a6454da050c619ea74e9
UglyNoob <q1253328221@163.com>
2024-03-16 19:34:18 +0800

    Wow

---
+++ publish.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import * as fs from 'node:fs/promises';
+
+
+async function main() {
+    await Promise.allSettled([
+        fs.mkdir("out/AmazingBP/scripts", { recursive: true }),
+        fs.mkdir("out/AmazingBP/structures/void", { recursive: true }),
+    ])
+
+    await Promise.all([
+        fs.copyFile("manifest.json", "out/AmazingBP/manifest.json"),
+        fs.copyFile("scripts/main.js", "out/AmazingBP/scripts/main.js"),
+        fs.copyFile("structures/void/void.mcstructure", "out/AmazingBP/structures/void/void.mcstructure")
+    ]);
+
+    console.log("Publish complete");
+}
+
+main();
